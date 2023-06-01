2ed9df59bfe18f176c3ca45c4836c89a3e9677cd
anumayagrawal12 <103452273+anumayagrawal12@users.noreply.github.com>
2023-06-02 01:59:45 +0530

    1 file modified

--- src/components/pages/Publications/Publications.jsx
+++ src/components/pages/Publications/Publications.jsx
@@ -1,66 +1,153 @@
 import React, { useState } from 'react';
 import './Publications.css';
 import { Link } from 'react-router-dom';
-// import Navbar from './Navbar';
-// import Footer from './components/pages/Footer';
-import Areas from '../Research/Research';
-import Project from '../Projects/Projects';
 import Drop from './Drop';
 import './Drop.css';
 export default function Publications() {
-  const [publishs, setpublish] = useState([
-    {
-      question: 'Dr. Dilip Singh Sisodia',
-      answer:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.',
-      open: false,
-    },
-    {
-      question: 'Deepak Singh',
-      answer: 'You! The viewer!',
-      open: false,
-    },
-    {
-      question: 'Sonal Yadav',
-      answer: 'This many!',
-      open: false,
-    },
-    {
-      question: 'Deepak Singh',
-      answer: 'You! The viewer!',
-      open: false,
-    },
-    {
-      question: 'Deepak Singh',
-      answer: 'You! The viewer!',
-      open: false,
-    },
-    {
-      question: 'Deepak Singh',
-      answer: 'You! The viewer!',
-      open: false,
-    },
-    {
-      question: 'Deepak Singh',
-      answer: 'You! The viewer!',
-      open: false,
-    },
-  ]);
+  // const [publishs, setpublish] = useState([
+  //   {
+  //     question: 'Dr. Dilip Singh Sisodia',
+  //     answer:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lorem eu dolor rhoncus, at scelerisque ligula gravida. Sed porta id mi sit amet convallis. Etiam iaculis massa sit amet lacus blandit sodales. Nulla ultrices velit a diam placerat congue. Pellentesque iaculis, ipsum quis eleifend dapibus, est dui eleifend ante, quis fermentum mi ligula quis nisl. Ut et ex dui. Integer id venenatis quam.',
+  //     open: false,
+  //   },
+  //   {
+  //     question: 'Deepak Singh',
+  //     answer: 'You! The viewer!',
+  //     open: false,
+  //   },
+  //   {
+  //     question: 'Sonal Yadav',
+  //     answer: 'This many!',
+  //     open: false,
+  //   },
+  //   {
+  //     question: 'Deepak Singh',
+  //     answer: 'You! The viewer!',
+  //     open: false,
+  //   },
+  //   {
+  //     question: 'Deepak Singh',
+  //     answer: 'You! The viewer!',
+  //     open: false,
+  //   },
+  //   {
+  //     question: 'Deepak Singh',
+  //     answer: 'You! The viewer!',
+  //     open: false,
+  //   },
+  //   {
+  //     question: 'Deepak Singh',
+  //     answer: 'You! The viewer!',
+  //     open: false,
+  //   },
+  // ]);
 
-  const toggledrop = (index) => {
-    setpublish(
-      publishs.map((publish, i) => {
-        if (i === index) {
-          publish.open = !publish.open;
-        } else {
-          publish.open = false;
-        }
+  // const toggledrop = (index) => {
+  //   setpublish(
+  //     publishs.map((publish, i) => {
+  //       if (i === index) {
+  //         publish.open = !publish.open;
+  //       } else {
+  //         publish.open = false;
+  //       }
 
-        return publish;
-      })
-    );
+  //       return publish;
+  //     })
+  //   );
+  // };
+  const year = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
+  const [selectedyear, setyear] = useState([2023]);
+  const selectHandler = (event) => {
+    setyear(event.target.value);
   };
-
+  const publish = [
+    {
+      year: 2023,
+      publishs: [
+        {
+          discription: 'hknhujbujv',
+          link: 'jbjbj',
+        },
+        {
+          discription: 'jbjbjb',
+          link: 'jojoj',
+        },
+        {
+          discription: 'jjbuj',
+          link: 'hihih',
+        },
+      ],
+    },
+    {
+      year: 2022,
+      publishs: [
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+      ],
+    },
+    {
+      year: 2021,
+      publishs: [
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+      ],
+    },
+    {
+      year: 2020,
+      publishs: [
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+      ],
+    },
+    {
+      year: 2019,
+      publishs: [
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+        {
+          discription: '',
+          link: '',
+        },
+      ],
+    },
+  ];
   return (
     <>
       <div className="main-box">
@@ -69,7 +156,6 @@ export default function Publications() {
             <div className="side-top-box">
               <p>Research Areas</p>
             </div>
-
             <ul className="side-link">
               <li>
                 <div className="side-border-left" />
@@ -87,7 +173,7 @@ export default function Publications() {
             </ul>
           </div>
         </div>
-        <div className="drops">
+        {/* <div className="drops">
           <h3>Publications</h3>
           {publishs.map((publish, index) => (
             <Drop
@@ -97,10 +183,41 @@ export default function Publications() {
               toggledrop={toggledrop}
             />
           ))}
+        </div> */}
+        <div>
+          <h3>Publications</h3>
+          <select name="Year">
+            {year.map((yr) => (
+              <option value={yr} onChange={selectHandler}>
+                {yr}
+              </option>
+            ))}
+          </select>
+          <div>
+            <h2>Year: {setyear}</h2>
+          </div>
+          <div>
+            {publish.map(({ year, publishs }) => (
+              <table>
+                <tr>
+                  <th>Index</th>
+                  <th>Publications</th>
+                  <th>Link</th>
+                </tr>
+                {publishs.map(({ description, link, index }) => (
+                  <tr>
+                    <td>{index}</td>
+                    <td>{description}</td>
+                    <td>
+                      <a href={link}>Link</a>
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            ))}
+          </div>
         </div>
       </div>
-      {/* <h1 className="consulting">Publications</h1> */}
-      {/* <Footer /> */}
     </>
   );
 }
