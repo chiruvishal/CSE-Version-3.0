7fcbcaff1f5883428d96dcf28330e858f98d6d78
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2023-06-07 12:26:14 +0530

    Added Carousel

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Banner from './Banner.jsx';
 import TestiMonials from './Testimonials.jsx';
-
+import NewsandAnnouncement from '../NewsAndAnnouncement/NewsAndAnnouncement';
+import NewsAndAnnouncement from '../NewsAndAnnouncement/NewsAndAnnouncement';
+import Events from '../Events/Events';
+import './Home.css';
+import MyCarousel from './MyCarousel';
 export default function Home() {
   return (
     <div>
-      <Banner/>
+      <MyCarousel />
+      <div className="mu">
+        <NewsAndAnnouncement />
+        <Events />
+      </div>
     </div>
   );
 }
