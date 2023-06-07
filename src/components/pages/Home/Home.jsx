9d981086d387756224d30cc94064820a73b1708f
anumayagrawal12 <103452273+anumayagrawal12@users.noreply.github.com>
2023-06-07 14:38:17 +0530

    1 file modified

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -3,10 +3,17 @@ import Banner from './Banner.jsx';
 import TestiMonials from './Testimonials.jsx';
 import NewsandAnnouncement from '../NewsAndAnnouncement/NewsAndAnnouncement';
 import NewsAndAnnouncement from '../NewsAndAnnouncement/NewsAndAnnouncement';
+import Events from '../Events/Events';
+import './Home.css';
+import MyCarousel from './MyCarousel';
 export default function Home() {
   return (
     <div>
-      <Banner />
+      <MyCarousel />
+      <div className="mu">
+        <NewsAndAnnouncement />
+        <Events />
+      </div>
     </div>
   );
 }
