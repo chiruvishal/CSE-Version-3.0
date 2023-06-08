10f7c2aa2dcb885c016d73b108327450b683b0a4
Mukesh751 <91366697+Mukesh751@users.noreply.github.com>
2023-06-09 03:31:44 +0530

    3 files modified

--- src/components/pages/Home/MyCarousel.jsx
+++ src/components/pages/Home/MyCarousel.jsx
@@ -20,30 +20,39 @@ function MyCarousel() {
     arrows: true,
   };
   return (
-    <div>
-      <Slider {...settings}>
-        <div className="row">
-          <img
-            src={cloud_mountains}
-            className="slide_img col-lg-12"
-            alt="Responsive img"
-          />
-        </div>
-        <div className="row">
-          <img
-            src={groups}
-            className="slide_img col-lg-12"
-            alt="Responsive img"
-          />
-        </div>
-        <div className="row">
-          <img
-            src={hills}
-            className="slide_img col-lg-12"
-            alt="Responsive img"
-          />
-        </div>
-      </Slider>
+    <div className="carsel">
+      <div className="part1">
+        <img
+          src={cloud_mountains}
+          className="slide_img col-lg-12"
+          alt="Responsive img"
+        />
+      </div>
+      <div className="part2">
+        <Slider {...settings}>
+          <div className="row">
+            <img
+              src={cloud_mountains}
+              className="slide_img col-lg-12"
+              alt="Responsive img"
+            />
+          </div>
+          <div className="row">
+            <img
+              src={groups}
+              className="slide_img col-lg-12"
+              alt="Responsive img"
+            />
+          </div>
+          <div className="row">
+            <img
+              src={hills}
+              className="slide_img col-lg-12"
+              alt="Responsive img"
+            />
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 }
