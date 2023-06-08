de42103ad7d9587bc9a9da1a407d3f5c9ce5f92f
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-06-08 20:55:26 +0530

    1 file modified, 2 added

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -6,10 +6,12 @@ import NewsAndAnnouncement from '../NewsAndAnnouncement/NewsAndAnnouncement';
 import Events from '../Events/Events';
 import './Home.css';
 import MyCarousel from './MyCarousel';
+import Welcome from './Welcome';
 export default function Home() {
   return (
     <div>
       <MyCarousel />
+      <Welcome/>
       <div className="mu">
         <NewsAndAnnouncement />
         <Events />
