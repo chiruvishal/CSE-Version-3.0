49a3fb6bb58fab74deb413b2c910e5178c784046
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-06-09 23:29:42 +0530

    1 file modified

--- src/components/pages/Faculty/Faculty.jsx
+++ src/components/pages/Faculty/Faculty.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 import FacultyCard from './FacultyCard';
 import './FacultyCard.css';
 export default function FacultyOverview() {
+  /// make the spi start from id =0
   const facultyMembers = [
     {
+      id: '0',
       name: 'Dr. Dilip Singh Sisodia ',
       picture: 'https://i.postimg.cc/fVdbRHgW/img1.png',
       Designition: 'Associate Professor',
@@ -14,13 +16,7 @@ export default function FacultyOverview() {
     },
 
     {
-      name: 'Dr. Dilip Singh Sisodia ',
-      picture: 'https://i.postimg.cc/fVdbRHgW/img1.png',
-      Designition: 'Associate Professor',
-      email: 'Email: dssisodia.cs@nitrr.ac.in',
-      phone: 'Phone: 8462808174',
-    },
-    {
+      id: '1',
       name: 'Dr. Naresh K. Nagwani',
       picture: 'https://i.postimg.cc/DWgnSRQh/img2.png',
       Designition: 'Associate Professor',
@@ -28,6 +24,7 @@ export default function FacultyOverview() {
       phone: 'Phone:9993312001',
     },
     {
+      id: '2',
       name: 'Dr. Pradeep Singh',
       picture: 'https://i.postimg.cc/r0wcxggr/img3.png',
       Designition: 'Associate Professor',
@@ -61,53 +58,53 @@ export default function FacultyOverview() {
             </div>
           </div>
           <div className="main-box">
-          <div className="side-box">
-            <div className="side-container">
-              <div className="side-top-box">
-                <p>Faculty</p>
+            <div className="side-box">
+              <div className="side-container">
+                <div className="side-top-box">
+                  <p>Faculty</p>
+                </div>
+
+                <ul className="side-link">
+                  <li>
+                    <div className="side-border-left" />
+                    <Link to="/Faculty">Associate Professor</Link>
+                  </li>
+                  <li>
+                    <div className="side-border-left" />
+                    <Link to="/AssistantProfessor">Assistant Professor</Link>
+                  </li>
+                  <li>
+                    <div className="side-border-left" />
+                    <Link to="/TempararyFaculty">Temparary Faculty</Link>
+                  </li>
+                  <li>
+                    <div className="side-border-left" />
+                    <Link to="/Staff">Staff</Link>
+                  </li>
+                </ul>
               </div>
+            </div>
+            <div className="drops">
+              <h3>Head Of The Department</h3>
 
-              <ul className="side-link">
-                <li>
-                  <div className="side-border-left" />
-                  <Link to="/Faculty">Associate Professor</Link>
-                </li>
-                <li>
-                  <div className="side-border-left" />
-                  <Link to="/AssistantProfessor">Assistant Professor</Link>
-                </li>
-                <li>
-                  <div className="side-border-left" />
-                  <Link to="/TempararyFaculty">Temparary Faculty</Link>
-                </li>
-                <li>
-                  <div className="side-border-left" />
-                  <Link to="/Staff">Staff</Link>
-                </li>
-              </ul>
+              <div>
+                {facultyMembers.map((faculty, index) => (
+                  <React.Fragment key={faculty.name}>
+                    <FacultyCard
+                      id={faculty.id}
+                      name={faculty.name}
+                      picture={faculty.picture}
+                      Designition={faculty.Designition}
+                      email={faculty.email}
+                      phone={faculty.phone}
+                    />
+
+                    {index === 0 && <h3>Associate Professor</h3>}
+                  </React.Fragment>
+                ))}
+              </div>
             </div>
           </div>
-          <div className="drops">
-            <h3>Head Of The Department</h3>
-
-            <div>
-              {facultyMembers.map((faculty, index) => (
-                <React.Fragment key={faculty.name}>
-                  <FacultyCard
-                    id={faculty.id}
-                    name={faculty.name}
-                    picture={faculty.picture}
-                    Designition={faculty.Designition}
-                    email={faculty.email}
-                    phone={faculty.phone}
-                  />
-
-                  {index === 0 && <h3>Associate Professor</h3>}
-                </React.Fragment>
-              ))}
-            </div>
-          </div>
-        </div>
           {/* <h1 className="consulting">Publications</h1> */}
           {/* <Footer /> */}
         </div>
