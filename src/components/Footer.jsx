bfdbec220a6c39d5969570308481c5708a048de9
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-06-14 00:02:50 +0530

    added madhuri files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ import { AiOutlineMail } from 'react-icons/ai';
 
 import { Link } from 'react-router-dom';
 
-
 import './Footer.css';
 const Footer = () => {
   const handleLinkClick = () => {
@@ -60,28 +59,29 @@ const Footer = () => {
               </div>
               <div className="col-md-121 col-lg-4">
                 <div className="contact-us">
-                <div className="contact-info">
-      <p className="contact-info-item">
-        <span className="iconf">
-          <TfiLocationPin size={32} />
-        </span>
-        National Institute of Technology Raipur G.E. Road, Raipur Chhatisgarh - 492010
-      </p>
-      <p className="contact-info-item">
-        <span className="iconf">
-          <AiOutlinePhone size={32} />
-        </span>
-        Phone:(+91)-771-2254200
-      </p>
-      <p>
-        <a href="#" className="contact-info-link">
-          <span className="iconf">
-            <AiOutlineMail size={32} />
-          </span>
-          Email- director@nitrr.ac.in registrar@nitrr.ac.in
-        </a>
-      </p>
-    </div>
+                  <div className="contact-info">
+                    <p className="contact-info-item">
+                      <span className="iconf">
+                        <TfiLocationPin size={32} />
+                      </span>
+                      National Institute of Technology Raipur G.E. Road, Raipur
+                      Chhatisgarh - 492010
+                    </p>
+                    <p className="contact-info-item">
+                      <span className="iconf">
+                        <AiOutlinePhone size={32} />
+                      </span>
+                      Phone:(+91)-771-2254200
+                    </p>
+                    <p>
+                      <a href="#" className="contact-info-link">
+                        <span className="iconf">
+                          <AiOutlineMail size={32} />
+                        </span>
+                        Email- director@nitrr.ac.in registrar@nitrr.ac.in
+                      </a>
+                    </p>
+                  </div>
                 </div>
                 <div className="footer-widget footer-left-widget">
                   <div className="section-heading">
@@ -89,48 +89,48 @@ const Footer = () => {
                     <span className="animate-border border-black"></span>
                   </div>
                   <ul>
-                  <li>
-                    <Link to="/" onClick={handleLinkClick}>
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/StudentSection" onClick={handleLinkClick}>
-                      Student Section
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/Gallery" onClick={handleLinkClick}>
-                      Gallery
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/Achievements" onClick={handleLinkClick}>
-                      Achievements
-                    </Link>
-                  </li>
-                </ul>
-                <ul>
-                  <li>
-                    <Link to="/Placements" onClick={handleLinkClick}>
-                      Placements
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/Course" onClick={handleLinkClick}>
-                      Course
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/Research-Areas" onClick={handleLinkClick}>
-                      Research Areas
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/ContactUs" onClick={handleLinkClick}>
-                      Contact Us
-                    </Link>
-                  </li>
+                    <li>
+                      <Link to="/" onClick={handleLinkClick}>
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/StudentSection" onClick={handleLinkClick}>
+                        Student Section
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/Gallery" onClick={handleLinkClick}>
+                        Gallery
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/Achievements" onClick={handleLinkClick}>
+                        Achievements
+                      </Link>
+                    </li>
+                  </ul>
+                  <ul>
+                    <li>
+                      <Link to="/Placements" onClick={handleLinkClick}>
+                        Placements
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/Course" onClick={handleLinkClick}>
+                        Course
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/Research-Areas" onClick={handleLinkClick}>
+                        Research Areas
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/ContactUs" onClick={handleLinkClick}>
+                        Contact Us
+                      </Link>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -147,7 +147,7 @@ const Footer = () => {
                     type="text/html"
                     width="635px"
                     height="413px"
-                    src="https://www.youtube.com/embed/tSQVgiwr3lU?autoplay=1&rel=0&modestbranding=1"
+                    src="https://www.youtube.com/embed/tSQVgiwr3lU?autoplay=1&rel=0&modestbranding=1&loop=1"
                     frameBorder="0"
                     allowFullScreen
                   />
