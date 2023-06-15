fa60c3e630012aceeb67c9ba8d9547c5515cfe31
anumayagrawal12 <anumayagrawal@gmail.com>
2023-06-16 00:24:40 +0530

    publication

--- src/components/pages/Projects/Projects.jsx
+++ src/components/pages/Projects/Projects.jsx
@@ -1,64 +1,64 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './Projects.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from "react";
+import "./Projects.css";
+import { Link } from "react-router-dom";
 export default function Projects() {
   const years = [2023, 2022];
   const [isVisible, setIsVisible] = useState(false);
-  const [Year, setYear] = useState('2023');
+  const [Year, setYear] = useState("2023");
   let currentyear = 2023;
   const sectionRef = useRef(null);
   const publish = [
     {
-      year: '2023',
+      year: "2023",
       publishs: [
         {
           description:
             'Domngam Boje, Mini Loya, Ananta Kumar Atta* "Amidoquinoline-based xylofuranose derivative for selective detection of Cu2+ in aqueous medium", Journal of Photochemistry and Photobiology A: Chemistry, vol. 437, pp. 114468, 2023.',
-          studentname: 'anumay',
-          professorname: 'Dr.dilip singh sisodia',
-          Technologis: 'React php',
-          link: 'jbjbj',
+          studentname: "anumay",
+          professorname: "Dr.dilip singh sisodia",
+          Technologis: "React php",
+          link: "jbjbj",
         },
         {
-          description: 'jbjbjb',
-          studentname: 'anumay',
-          professorname: 'Dr.dilip singh sisodia',
-          Technologis: 'React php',
-          link: 'jojoj',
+          description: "jbjbjb",
+          studentname: "anumay",
+          professorname: "Dr.dilip singh sisodia",
+          Technologis: "React php",
+          link: "jojoj",
         },
         {
-          description: 'jjbuj',
-          studentname: 'anumay',
-          professorname: 'Dr.dilip singh sisodia',
-          Technologis: 'React php',
-          link: 'hihih',
+          description: "jjbuj",
+          studentname: "anumay",
+          professorname: "Dr.dilip singh sisodia",
+          Technologis: "React php",
+          link: "hihih",
         },
       ],
     },
     {
-      year: '2022',
+      year: "2022",
       publishs: [
         {
           description:
             'Domngam Boje, Mini Loya, anuanta Kumar Atta* "Amidoquinoline-based xylofuranose derivative for selective detection of Cu2+ in aqueous medium", Journal of Photochemistry and Photobiology A: Chemistry, vol. 437, pp. 114468, 2023.',
-          studentname: 'anumay',
-          professorname: 'Dr.dilip singh sisodia',
-          Technologis: 'React php',
-          link: 'jbjbj',
+          studentname: "anumay",
+          professorname: "Dr.dilip singh sisodia",
+          Technologis: "React php",
+          link: "jbjbj",
         },
         {
-          description: 'jbjbjb',
-          studentname: 'anumay',
-          professorname: 'Dr.dilip singh sisodia',
-          Technologis: 'React php',
-          link: 'jojoj',
+          description: "jbjbjb",
+          studentname: "anumay",
+          professorname: "Dr.dilip singh sisodia",
+          Technologis: "React php",
+          link: "jojoj",
         },
         {
-          description: 'jjbuj',
-          studentname: 'anumay',
-          professorname: 'Dr.dilip singh sisodia',
-          Technologis: 'React php',
-          link: 'hihih',
+          description: "jjbuj",
+          studentname: "anumay",
+          professorname: "Dr.dilip singh sisodia",
+          Technologis: "React php",
+          link: "hihih",
         },
       ],
     },
@@ -95,11 +95,11 @@ export default function Projects() {
   }, []);
   return (
     <>
-    <div>
+      <div>
         <div
           className="nitr-page-title-wrap nitr-style-custom nitr-left-align"
           style={{
-            backgroundImage: 'url(https://i.postimg.cc/yNBbxWnQ/IMG-2618.jpg)',
+            backgroundImage: "url(https://i.postimg.cc/yNBbxWnQ/IMG-2618.jpg)",
           }}
         >
           <div className="nitr-header-transparent-substitute "></div>
@@ -107,89 +107,90 @@ export default function Projects() {
           <div className="nitr-page-title-container green destinations-section-wrapper nitr-container ">
             <div
               className="nitr-page-title-content nitr-item-pdlr"
-              style={{ paddingBottom: '60px' }}
+              style={{ paddingBottom: "60px" }}
             >
               <div className="green-line-text">Projects</div>
-
             </div>
           </div>
         </div>
-      <div className="main-box" ref={sectionRef}>
-        <div className="side-box">
-          <div className="side-container">
-            <div className="side-top-box">
-              <p>Research Areas</p>
+        <div className="main-box" ref={sectionRef}>
+          <div className="side-box">
+            <div className="side-container">
+              <div className="side-top-box">
+                <p>Research Areas</p>
+              </div>
+              <ul className="side-link">
+                <li>
+                  <div className="side-border-left" />
+                  <Link to="/Research-Areas">Areas</Link>
+                </li>
+                <li>
+                  <div className="side-border-left" />
+                  <Link to="/Project">Project</Link>
+                </li>
+                <li>
+                  {" "}
+                  <div className="side-border-left" />
+                  <Link to="/Publications">Publications</Link>
+                </li>
+              </ul>
             </div>
-            <ul className="side-link">
-              <li>
-                <div className="side-border-left" />
-                <Link to="/Research-Areas">Areas</Link>
-              </li>
-              <li>
-                <div className="side-border-left" />
-                <Link to="/Project">Project</Link>
-              </li>
-              <li>
-                {' '}
-                <div className="side-border-left" />
-                <Link to="/Publications">Publications</Link>
-              </li>
-            </ul>
           </div>
-        </div>
-        <div className={`publication ${
-              isVisible ? 'animate__animated animate__slideInRight' : ''
-            }`}>
-          <h3 className="he">Projects</h3>
-          <div className="maint">
-            <div className="selectyr">
-              <div className="selectyeasr">
-                <select name="Year" id="sel" onChange={selectHandler}>
-                  <option value="2023">--Select Year--</option>
-                  {years.map((yr) => (
-                    <option value={yr}>{yr}</option>
-                  ))}
-                </select>
+          <div
+            className={`project ${
+              isVisible ? "animate__animated animate__slideInRight" : ""
+            }`}
+          >
+            <h3 className="he">Projects</h3>
+            <div className="maint">
+              <div className="selectyr">
+                <div className="selectyeasr">
+                  <select name="Year" id="sel" onChange={selectHandler}>
+                    <option value="2023">--Select Year--</option>
+                    {years.map((yr) => (
+                      <option value={yr}>{yr}</option>
+                    ))}
+                  </select>
+                </div>
+                <div className="syear">
+                  <h2>Year: {Year}</h2>
+                </div>
               </div>
-              <div className="syear">
-                <h2>Year: {Year}</h2>
-              </div>
-            </div>
-            <div className="pubtable">
-              {publish.map(({ year, publishs }) =>
-                year === Year ? (
-                  <table className="publishtable">
-                    <thead>
-                      <tr>
-                        <th className="wid">S.no.</th>
-                        <th className="pwid">Projects</th>
-                        <th className="wid2">Students Name</th>
-                        <th className="wid2">Professor</th>
-                        <th className="wid">Technologies used</th>
-                        {/* <th className="wid">Link</th> */}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {publishs.map((pub, idx) => (
+              <div className="protable">
+                {publish.map(({ year, publishs }) =>
+                  year === Year ? (
+                    <table className="projecttable">
+                      <thead>
                         <tr>
-                          <td>{idx + 1}</td>
-                          <td>{pub.description}</td>
-                          <td>{pub.studentname}</td>
-                          <td>{pub.professorname}</td>
-                          <td>{pub.Technologis}</td>
-                          {/* <td>
+                          <th className="wid">S.no.</th>
+                          <th className="pwid">Projects</th>
+                          <th className="wid2">Students Name</th>
+                          <th className="wid2">Professor</th>
+                          <th className="wid">Technologies used</th>
+                          {/* <th className="wid">Link</th> */}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {publishs.map((pub, idx) => (
+                          <tr>
+                            <td>{idx + 1}</td>
+                            <td>{pub.description}</td>
+                            <td>{pub.studentname}</td>
+                            <td>{pub.professorname}</td>
+                            <td>{pub.Technologis}</td>
+                            {/* <td>
                             <a href={pub.link}>Link</a>
                           </td> */}
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                ) : null
-              )}
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  ) : null
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
