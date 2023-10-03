4a062223b9dd573a36294346907a4e13caed0fd6
unknown <shriyanshgaur55@gmail.com>
2023-10-03 14:27:00 +0530

    photos

--- src/components/pages/Home/Welcome.jsx
+++ src/components/pages/Home/Welcome.jsx
@@ -12,7 +12,7 @@ const DirectorMessage = () => {
         <div className="flexw flex-colw space-y-6 rounded-lg bg-light-purple overflow-hidden shadow-md md:flex-row md:space-y-0 md:space-x-6">
           <div className="basis-4/12">
             <img
-              src="https://i.postimg.cc/NfpbyZsW/Mr-Pradeep-Singh.jpg"
+              src="https://i.postimg.cc/qMrJWV55/p.jpg"
               className="basis-4/12 h-full w-full object-cover hodimg"
               alt="Director's Image"
             />
